121e72b86e5fb8d87c4c24b84cb06faa095a673e
amisha26 <amisha8299@gmail.com>
2021-10-08 12:16:33 +0530

    error in todoreducer

--- src/comp/Todo.js
+++ src/comp/Todo.js
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { addTodo, deleteTodo, removeTodo } from "../actions/index";
 
 const Todo = () => {
   const [inputData, setInputData] = useState("");
-
+  const list = useSelector((state) => state.todoReducers.list);
   const dispatch = useDispatch();
 
   return (
@@ -24,6 +24,22 @@ const Todo = () => {
             className="fa fa-plus add-btn"
             onClick={() => dispatch(addTodo(inputData), setInputData(""))}
           ></i>
+        </div>
+        <div className="showItems">
+          {list.map((elem) => {
+            return (
+              <div className="eachItem" key={elem.id}>
+                <h3>{elem.data}</h3>
+                <div className="todo-btn">
+                  <i
+                    className="far fa-trash-alt add-btn"
+                    title="Delete Item"
+                    onClick={() => dispatch(deleteTodo(elem.id))}
+                  ></i>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
